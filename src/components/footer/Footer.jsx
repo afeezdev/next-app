d34d10477e608ee078b6cd2e3668183640b37eff
afeezdev <afeezdev@gmail.com>
2024-01-25 02:07:25 +0000

    {

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,9 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>lamadev</div>
+      <div className={styles.logo}>afeezdev</div>
       <div className={styles.text}>
-        Lama creative thoughts agency © All rights reserved.
+        Afeezdev © 2024.
       </div>
     </div>
   );
