77991ddc3e0cfc3e8868953075bd79619e5ed077
afeezdev <afeezdev@gmail.com>
2024-01-25 00:16:39 +0000

    added login and register page

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,7 +1,14 @@
-const RegisterPage = () => {
-    return(
-        <div>RegisterPage</div>
-    )
-}
+import styles from "./register.module.css";
+import RegisterForm from "@/components/registerForm/registerForm";
 
-export default RegisterPage
+const RegisterPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <RegisterForm/>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
